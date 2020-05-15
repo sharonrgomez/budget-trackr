e3a3914f6fefda3f557dc79a8540c8a7d88982ca
Sharon <srcbun@gmail.com>
2020-05-15 15:33:29 -0400

    set up add expense form

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ExpenseForm from "./ExpenseForm";
+import { connect } from "react-redux";
+import { addExpense } from "../actions/expenses";
 
-const AddExpensePage = () => (
+const AddExpensePage = (props) => (
     <div>
         <h1>Add Expense</h1>
-        <ExpenseForm />
+        <ExpenseForm
+            onSubmit={(expense) => {
+                props.dispatch(addExpense(expense));
+                props.history.push("/");
+            }} />
     </div>
 );
 
-export default AddExpensePage;
+export default connect()(AddExpensePage);
--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -13,7 +13,8 @@ export default class ExpenseForm extends React.Component {
         note: "",
         amount: "",
         createdAt: moment(),
-        calendarFocused: false
+        calendarFocused: false,
+        error: ""
     };
 
     onDescriptionChange = (e) => {
@@ -27,23 +28,42 @@ export default class ExpenseForm extends React.Component {
 
     onAmountChange = (e) => {
         const amount = e.target.value;
-        if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+        if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
             this.setState(() => ({ amount }));
         }
     };
 
     onDateChange = (createdAt) => {
-        this.setState(() => ({ createdAt }));
+        if (createdAt) {
+            this.setState(() => ({ createdAt }));
+        }
     };
 
     onFocusChange = ({ focused }) => {
         this.setState(() => ({ calendarFocused: focused }));
     };
 
+    onSubmit = (e) => {
+        e.preventDefault();
+
+        if (!this.state.description || !this.state.amount) {
+            this.setState(() => ({ error: "Please provide a description and an amount." }));
+        } else {
+            this.setState(() => ({ error: "" }));
+            this.props.onSubmit({
+                description: this.state.description,
+                amount: parseFloat(this.state.amount, 10) * 100,
+                createdAt: this.state.createdAt.valueOf(),
+                note: this.state.note
+            });
+        }
+    };
+
     render() {
         return (
             <div>
-                <form>
+                {this.state.error && <p>{this.state.error}</p>}
+                <form onSubmit={this.onSubmit}>
                     <input
                         type="text"
                         placeholder="description"
