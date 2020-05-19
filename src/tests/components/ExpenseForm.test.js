5876b33b3fb65287ffeaefceca30853ef7764349
Sharon <srcbun@gmail.com>
2020-05-19 11:21:02 -0400

    complete testing of ExpenseForm component

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -85,3 +85,9 @@ test("should set new date on date change", () => {
     expect(wrapper.state("createdAt")).toEqual(now);
 });
 
+test("should set calendar focus on change", () => {
+    const focused = true;
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find(SingleDatePicker).prop("onFocusChange")({focused});
+    expect(wrapper.state("calendarFocused")).toEqual(focused);
+});
