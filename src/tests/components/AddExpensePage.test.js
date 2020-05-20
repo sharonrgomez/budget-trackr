6ddc4cfd9f8d6d7f2b003e85eeaca309a0d8bb0a
Sharon <srcbun@gmail.com>
2020-05-20 16:32:17 -0400

    change variable names to reflect action generators for addexpensepage test

--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -3,19 +3,21 @@ import { shallow } from "enzyme";
 import { AddExpensePage } from "../../components/AddExpensePage";
 import expenses from "../fixtures/expenses";
 
+let addExpense, history, wrapper;
+
+beforeEach(() => {
+    addExpense = jest.fn();
+    history = { push: jest.fn() };
+    wrapper = shallow(<AddExpensePage addExpense={addExpense} history={history} />);
+});
+
 test("should render add expense page", () => {
-    const onSubmit = jest.fn();
-    const history = { push: jest.fn() };
-    const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
     expect(wrapper).toMatchSnapshot();
 });
 
 test("should handle onSubmit", () => {
-    const onSubmit = jest.fn();
-    const history = { push: jest.fn() };
-    const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
     wrapper.find("ExpenseForm").prop("onSubmit")(expenses[1]);
     expect(history.push).toHaveBeenLastCalledWith("/");
-    expect(onSubmit).toHaveBeenLastCalledWith(expenses[1]);
+    expect(addExpense).toHaveBeenLastCalledWith(expenses[1]);
 
 }); 
