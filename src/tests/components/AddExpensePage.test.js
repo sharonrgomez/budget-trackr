83e53885291a079fae5aecc253eb06daa4baedad
Sharon <srcbun@gmail.com>
2020-05-20 15:15:35 -0400

    fix addexpensepage page render tests & fix expense form test importing singledatepicker

--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 import { AddExpensePage } from "../../components/AddExpensePage";
+import expenses from "../fixtures/expenses";
 
 test("should render add expense page", () => {
     const onSubmit = jest.fn();
@@ -13,5 +14,8 @@ test("should handle onSubmit", () => {
     const onSubmit = jest.fn();
     const history = { push: jest.fn() };
     const wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
-    wrapper.find("ExpenseForm").prop("onSubmit")();
-});
+    wrapper.find("ExpenseForm").prop("onSubmit")(expenses[1]);
+    expect(history.push).toHaveBeenLastCalledWith("/");
+    expect(onSubmit).toHaveBeenLastCalledWith(expenses[1]);
+
+}); 
--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -3,7 +3,6 @@ import { shallow } from "enzyme";
 import ExpenseForm from "../../components/ExpenseForm";
 import expenses from "../fixtures/expenses";
 import moment from "moment";
-import { SingleDatePicker } from "react-dates";
 
 
 test("should render expense form", () => {
@@ -81,13 +80,13 @@ test("should set new date on date change", () => {
     const now = moment();
     const wrapper = shallow(<ExpenseForm />);
 
-    wrapper.find(SingleDatePicker).prop("onDateChange")(now);
+    wrapper.find("withStyles(SingleDatePicker)").prop("onDateChange")(now);
     expect(wrapper.state("createdAt")).toEqual(now);
 });
 
 test("should set calendar focus on change", () => {
     const focused = true;
     const wrapper = shallow(<ExpenseForm />);
-    wrapper.find(SingleDatePicker).prop("onFocusChange")({focused});
+    wrapper.find("withStyles(SingleDatePicker)").prop("onFocusChange")({focused});
     expect(wrapper.state("calendarFocused")).toEqual(focused);
 });
