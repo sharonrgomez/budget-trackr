af68c905c9d4f0a2e085ccc5bcb7bc61a615e791
Sharon <srcbun@gmail.com>
2020-05-21 10:07:49 -0400

    test edit expense and remove expense handlers for editexpensepage component

--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -19,5 +19,4 @@ test("should handle onSubmit", () => {
     wrapper.find("ExpenseForm").prop("onSubmit")(expenses[1]);
     expect(history.push).toHaveBeenLastCalledWith("/");
     expect(addExpense).toHaveBeenLastCalledWith(expenses[1]);
-
 }); 
