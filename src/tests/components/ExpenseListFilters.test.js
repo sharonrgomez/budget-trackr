4ebe2337854fd3c8571d5e855a3bab2e80ab0fa8
Sharon <srcbun@gmail.com>
2020-05-22 11:55:05 -0400

    test expenselistfilters component's handlers

--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 import { filters, altFilters } from "../fixtures/filters";
 import { ExpenseListFilters } from "../../components/ExpenseListFilters";
+import moment from "moment";
 
 let setTextFilter, setStartDate, setEndDate, sortByAmount, sortByDate, wrapper;
 
@@ -31,4 +32,46 @@ test("should render expenselistfilters with given data", () => {
         filters: altFilters
     });
     expect(wrapper).toMatchSnapshot();
+});
+
+test("should handle text change", () => {
+    const value = "water";
+    wrapper.find("input").simulate("change", {
+        target: { value }
+    });
+    expect(setTextFilter).toHaveBeenCalledWith(value);
+});
+
+test("should handle sortbyamount", () => {
+    const value = "amount";
+    wrapper.find("select").simulate("change", {
+        target: { value }
+    });
+    expect(sortByAmount).toHaveBeenCalled();
+});
+
+test("should handle sortbydate", () => {
+    const value = "date";
+    wrapper.setProps({
+        filters: altFilters
+    });
+    wrapper.find("select").simulate("change", {
+        target: { value }
+    });
+    expect(sortByDate).toHaveBeenCalled();
+});
+
+test("should handle date changes", () => {
+    const startDate = moment(0).add(5, "days");
+    const endDate = moment(0).add(20, "days");
+    wrapper.find("withStyles(DateRangePicker)").prop("onDatesChange")({ startDate, endDate });
+    expect(setStartDate).toHaveBeenLastCalledWith(startDate);
+    expect(setEndDate).toHaveBeenLastCalledWith(endDate);
+
+});
+
+test("should handle date focus changes", () => {
+    const calendarFocused = "endDate";
+    wrapper.find("withStyles(DateRangePicker)").prop("onFocusChange")(calendarFocused);
+    expect(wrapper.state("calendarFocused")).toBe(calendarFocused);
 });
