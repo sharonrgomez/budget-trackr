5f9824733213ce0fba2ecbdec373d42bdaa487bf
Sharon <srcbun@gmail.com>
2020-05-22 12:52:17 -0400

    refactor webpack.config.js to optimize build for production

--- src/app.js
+++ src/app.js
@@ -11,38 +11,7 @@ import 'react-dates/initialize';
 import "react-dates/lib/css/_datepicker.css";
 
 const store = configureStore();
-
-store.dispatch(addExpense({
-    description: "Water bill",
-    amount: 4500,
-    createdAt: 1000
-}));
-
-store.dispatch(addExpense({
-    description: "Electricity bill",
-    amount: 6000,
-    createdAt: -1500
-}));
-
-store.dispatch(addExpense({
-    description: "May Rent",
-    amount: 25000,
-    createdAt: 500
-})); 
-
-store.dispatch(addExpense({
-    description: "Medium Coffee",
-    amount: 250
-})); 
-
-store.dispatch(addExpense({
-    description: "Large Banana Smoothie",
-    amount: 500
-}));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
+console.log("hi");
 const jsx = (
     <Provider store={store}>
         <AppRouter />
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,39 @@
 const path = require("path");
 
-module.exports = {
-    entry: "./src/app.js",
-    output: {
-        path: path.join(__dirname, "public"),
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [{
-            loader: "babel-loader",
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                "style-loader",
-                "css-loader",
-                "sass-loader"
-            ] 
-        }]
-    },
-    devtool: "cheap-module-eval-source-map",
-    devServer: {
-        contentBase: path.join(__dirname, "public"),
-        historyApiFallback: true,    // always send user to index.html, let react-router decide which component to render
-        liveReload: true,
-        lazy: false
-    },
-    mode: "development"
+module.exports = (env) => {
+    const isProd = env === "production";
+    return {
+        entry: "./src/app.js",
+        output: {
+            path: path.join(__dirname, "public"),
+            filename: "bundle.js"
+        },
+        module: {
+            rules: [{
+                loader: "babel-loader",
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            }]
+        },
+        devtool: isProd ? "source-map" : "cheap-module-eval-source-map",
+        devServer: {
+            contentBase: path.join(__dirname, "public"),
+            historyApiFallback: true    // always send user to index.html, let react-router decide which component to render
+            // liveReload: true,
+            // lazy: false
+        },
+        performance: {
+            hints: false,
+            maxEntrypointSize: 512000,
+            maxAssetSize: 512000
+        }
+        // mode: "development"
+    }
 };
