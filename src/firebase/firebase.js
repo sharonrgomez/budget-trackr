cee3eaa868c98c92282b5a2e803091d772c53dc4
Sharon <srcbun@gmail.com>
2020-06-03 14:30:23 -0400

    set up test database env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBj4b9-jCBWfTv6E8PzGLVuEhfzS0VzNMA",
-    authDomain: "expenses-trackr.firebaseapp.com",
-    databaseURL: "https://expenses-trackr.firebaseio.com",
-    projectId: "expenses-trackr",
-    storageBucket: "expenses-trackr.appspot.com",
-    messagingSenderId: "968721836635",
-    appId: "1:968721836635:web:3fdc2b043b75fdf5a32d7e"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -58,9 +58,9 @@ export { firebase, database as default };
 //     console.log("unsubscribed from data fetching");
 // }, 2000);
 
-// // database.ref().update({
-// //     "job/company": "google"
-// // });
+// database.ref().update({
+//     "job/company": "google"
+// });
 
 // const name = database.ref().on("value", (snapshot) => {
 //     const val = snapshot.val();
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,14 @@
 const path = require("path");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require("webpack");
+
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+if (proces.env_NODE_ENV == "test") {
+    require("dotenv").config({ path: ".env.test" });
+} else if (process.env.NODE_ENV === "development") {
+    require("dotenv").config({ path: ".env.development" });
+}
 
 module.exports = (env) => {
     const isProd = env === "production";
@@ -36,7 +45,16 @@ module.exports = (env) => {
             }]
         },
         plugins: [
-            CSSExtract
+            CSSExtract,
+            new webpack.DefinePlugin({
+                "process.env.FIREBASE_API_KEY": JSON.stringify(process.env.FIREBASE_API_KEY),
+                "process.env.FIREBASE_AUTH_DOMAIN": JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+                "process.env.FIREBASE_DATABASE_URL": JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+                "process.env.FIREBASE_PROJECT_ID": JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+                "process.env.FIREBASE_STORAGE_BUCKET": JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+                "process.env.FIREBASE_MESSAGING_SENDER_ID": JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+                "process.env.FIREBASE_APP_ID": JSON.stringify(process.env.FIREBASE_APP_ID)
+            })
         ],
         devtool: isProd ? "source-map" : "inline-source-map",
         devServer: {
