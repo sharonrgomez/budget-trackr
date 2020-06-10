f3a46467e8d75e576e5a618443e96d42ed595fa5
Sharon <srcbun@gmail.com>
2020-06-10 17:59:29 -0400

    add babel polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = (env) => {
     const isProd = env === "production";
     const CSSExtract = new ExtractTextPlugin("styles.css");
     return {
-        entry: "./src/app.js",
+        entry: ["@babel/polyfill", "./src/app.js"],
         output: {
             path: path.join(__dirname, "public", "dist"),
             filename: "bundle.js"
