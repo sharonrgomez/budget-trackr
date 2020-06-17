e4690b252e224536101ded129649234c7cd1253b
Sharon <srcbun@gmail.com>
2020-06-17 10:06:18 -0400

    add link to my github profile

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { startLogout } from "../actions/auth";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCode } from '@fortawesome/free-solid-svg-icons'
 
 export const Header = ({ startLogout }) => (
     <header className="header">
         <div className="content-container">
             <div className="header__content">
                 <Link className="header__title" to="/dashboard"><h1>budget trackr</h1></Link>
-                <button className="button button__link" onClick={startLogout}>Logout</button>
+                <div>
+                    <a href="https://github.com/shaerins" target="_blank"><FontAwesomeIcon className="header-icon" icon={faCode} /></a>
+                    <button className="button button__link" onClick={startLogout}>Logout</button>
+                </div>
             </div>
         </div>
     </header>
